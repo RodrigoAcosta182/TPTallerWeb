8edb82683c0330335603bbb41eb93aa5e40d6559
julioRodrigoAcosta <jracosta1991@gmail.com>
2021-10-21 20:16:53 -0300

    mover ver publicacion a publicacion

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionImpl.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import java.io.File;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 @Service("servicioPublicacion")
 @Transactional
@@ -36,6 +40,7 @@ public class ServicioPublicacionImpl implements ServicioPublicacion {
             throw new Exception();
         return repositorioPublicacion.buscarTodasLasPublicacionesPerdidas();
     }
+
 
     @Override
     public List<Publicacion> listarTodasLasPublicacionesEncontradas() throws Exception {
@@ -85,4 +90,44 @@ public class ServicioPublicacionImpl implements ServicioPublicacion {
 
         return nuevaPublicacion;
     }
+
+    @Override
+    public void enviarCorreo(String receptor, String comentario) throws Exception {
+
+        if(receptor == null || comentario == null)
+            throw new Exception();
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String miCuenta = "missingpetsunlam@gmail.com";
+        String password = "Unlam2021";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(miCuenta, password);
+            }
+        });
+
+        Message mensaje = prepararMensaje(session, miCuenta, receptor, comentario);
+        Transport.send(mensaje, mensaje.getRecipients(Message.RecipientType.TO));
+    }
+
+    private static Message prepararMensaje(Session session, String miCuenta,String receptor,  String comentario) {
+        try {
+            MimeMessage mensaje = new MimeMessage(session);
+            mensaje.setFrom(new InternetAddress(miCuenta));
+            mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(receptor));
+            mensaje.setSubject("Missing Pets");
+            mensaje.setText("Recibiste un mensaje de la APP Missing Pets: " + comentario);
+            return mensaje;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
