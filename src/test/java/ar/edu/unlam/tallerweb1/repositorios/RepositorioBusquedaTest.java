6f19bbb6ffd56f2e46b1ae8085bf4bcd55864cbe
julioRodrigoAcosta <jracosta1991@gmail.com>
2021-11-07 14:34:40 -0300

    buscar por Localidad / Fix Test Repo

--- src/test/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioBusquedaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioBusquedaTest.java
@@ -2,17 +2,20 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.modelo.Localidad;
+import ar.edu.unlam.tallerweb1.modelo.Publicacion;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RepositorioBusquedaTest extends SpringTest {
 
+    private static final Publicacion PUBLICACION = new Publicacion(new Localidad("Moron"));
 
     @Autowired
     private RepositorioBusqueda repositorioBusqueda;
@@ -29,17 +32,44 @@ public class RepositorioBusquedaTest extends SpringTest {
         thenObtengoTodasLaslocalidades(localidades.size(),localidadesObtenidas);
     }
 
-    private void thenObtengoTodasLaslocalidades(int cantidadEsperada, List<Localidad> localidadesObtenidas) {
-        assertThat(localidadesObtenidas).hasSize(cantidadEsperada);
+    @Test
+    @Transactional
+    @Rollback
+    public void buscoPublicacionesPorLocalidadExitosamente(){
+        List<Publicacion> listaPublicaciones = new LinkedList<>();
+        listaPublicaciones.add(PUBLICACION);
+
+        givenQueExistenPublicaciones(listaPublicaciones);
+        List<Publicacion> publicacionesEncontradas = whenBuscoPublicacionesPorLocalidad(PUBLICACION);
+        thenEncuentroPublicaciones(publicacionesEncontradas.size(),listaPublicaciones);
     }
 
-    private List<Localidad> whenObtengoTodasLasLocalidades() {
-        return repositorioBusqueda.obtenerTodasLasLocalidades();
+    private void thenEncuentroPublicaciones(int cantidadEsperada, List<Publicacion> listaPublicaciones) {
+        assertThat(listaPublicaciones).hasSize(cantidadEsperada);
     }
+
+    private List<Publicacion> whenBuscoPublicacionesPorLocalidad(Publicacion publicacion) {
+        return repositorioBusqueda.obtenerPublicacionesPorLocalidad(publicacion.getLocalidad().getDescripcion());
+    }
+
+    private void givenQueExistenPublicaciones(List<Publicacion> listaPublicaciones) {
+        for(Publicacion publicacion: listaPublicaciones){
+            session().save(publicacion);
+        }
+    }
+
 
     private void givenExistenLocalidades(List<Localidad> localidades) {
         for (Localidad localidad : localidades){
             session().save(localidad);
         }
     }
+    private List<Localidad> whenObtengoTodasLasLocalidades() {
+        return repositorioBusqueda.obtenerTodasLasLocalidades();
+    }
+    private void thenObtengoTodasLaslocalidades(int cantidadEsperada, List<Localidad> localidadesObtenidas) {
+        assertThat(localidadesObtenidas).hasSize(cantidadEsperada);
+    }
+
+
 }
